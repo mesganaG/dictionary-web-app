b32df4cddede896c94d6ffc4eb25e0bf4516f512
Mesgana Geletu <121146669+mesganaG@users.noreply.github.com>
2023-11-11 03:01:42 +0000

    Compeleted the search bar

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const fontStyle = document.querySelector('.dropdown-item');
+function fontUpdate(){
+    const fontStyle = document.querySelectorAll('.dropdown-item');
+    const dropdownBtn = document.querySelector('#dropdown-btn');
+    for(let font of fontStyle){
+    font.addEventListener('click', function(){
+       const fontName = font.textContent;    
+       dropdownBtn.innerText = fontName;
+    });
+    }
 
-fontStyle.addEventListener('click', ()  => {
-    const dropdownBtn = document.getElementById('dropdown-btn');
-    dropdownBtn.innerText = fontStyle.innerText;
+}
+function searchOutlineUpdate(){
+    const inputText = document.querySelector('.search-bar-input');
+    const inputContainer = document.querySelector('.search-bar');
+    const searchBtn = document.querySelector('.search-btn');
+    const displayError = document.querySelector('.display-error');
+    inputText.addEventListener('click', function(){
+    inputContainer.style.border = '1px solid #A445ED';
+    
+    searchBtn.addEventListener('click', function(){
+        if(inputText.value === ''){
+            inputContainer.style.border = '1px solid #FF5252';
+            displayError.style.display = 'block';
+
+        } else {
+            inputContainer.style.border = '0px';
+           
+        }
+    });
+    displayError.style.display = 'none';
+
 });
+}
+
+
+fontUpdate();
+searchOutlineUpdate();
