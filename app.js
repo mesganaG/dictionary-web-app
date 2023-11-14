f88f805e5789799b5fe3d34bbdec3232f30d9a62
Mesgana Geletu <121146669+mesganaG@users.noreply.github.com>
2023-11-14 20:54:59 +0000

    Compeleted responsiveness and font styles

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 function fontUpdate() {
   const fontStyle = document.querySelectorAll(".dropdown-item");
   const dropdownBtn = document.querySelector("#dropdown-btn");
+  window.onload = dropdownBtn.innerText = "Sans Serif";
   for (let font of fontStyle) {
     font.addEventListener("click", function () {
-      const fontName = font.textContent;
-      dropdownBtn.innerText = fontName;
+      dropdownBtn.innerText = font.textContent;
+      const currFont = font.textContent;
+      if (currFont === "Serif") {
+        document.body.style.fontFamily = "serif";
+        dropdownBtn.style.fontFamily = "serif";
+      } else if (currFont === "Mono") {
+        document.body.style.fontFamily = "monospace";
+        dropdownBtn.style.fontFamily = "monospace";
+      } else {
+        document.body.style.fontFamily = "sans-serif";
+        dropdownBtn.style.fontFamily = "sans-serif";
+      }
     });
   }
 }
-function searchOutlineUpdate() {
+
+function SearchBoxEffect() {
   const inputText = document.querySelector(".search-bar-input");
   const inputContainer = document.querySelector(".search-bar");
   const searchBtn = document.querySelector(".search-btn");
   const displayError = document.querySelector("p");
   inputText.addEventListener("click", function () {
+    
     inputContainer.style.border = "1px solid #A445ED";
-
     searchBtn.addEventListener("click", function () {
       if (inputText.value === "") {
         inputContainer.style.border = "1px solid #FF5252";
@@ -27,7 +39,6 @@ function searchOutlineUpdate() {
     displayError.style.display = "none";
   });
 }
-
 function playBtnHoverEffect() {
   const playBtnImg = document.querySelector(".btn-img");
 
@@ -39,16 +50,24 @@ function playBtnHoverEffect() {
     playBtnImg.src = "./assets/images/icon-play.svg";
   });
 }
+// Dark mode
+function DarkMode() {
+  const darkModeBtn = document.querySelector("#flexSwitchCheckDefault");
+  darkModeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
 
+    if (darkModeBtn.checked === true) {
+      document.querySelector(".moon-icon").src =
+        "./assets/images/darkmode_half-moon.svg";
+    } else {
+      document.querySelector(".moon-icon").src =
+        "./assets/images/icon-moon.svg";
+    }
+  });
+}
 
-// Dark mode 
-
-const darkModeBtn = document.querySelector("#flexSwitchCheckDefault");
-
-
-darkModeBtn.addEventListener("click", function () {
-     document.body.classList.toggle("dark-mode");
-});
 fontUpdate();
-searchOutlineUpdate();
+SearchBoxEffect();
 playBtnHoverEffect();
+
+DarkMode();
